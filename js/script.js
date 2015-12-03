416368a13288cf48069be8c3196729f1f8142a80
BCP27 <gamemeister27@gmail.com>
2015-12-02 21:41:06 -0600

    done

--- js/script.js
+++ js/script.js
@@ -16,29 +16,50 @@ $(document).ready(function() {
     
     var update = $("#weather");
     var divup = $("<div>");
+    var type = $("<h2>");
+    var temp = $("<h2>");
+    var pup = $("<p>");
+    var pup1 = $("<p>");
+    type.text("Weather Type");
+    temp.text("Temperature (f)");
     divup.attr("id", "newweath");
     update.append(divup);
+    divup.append(type);
+    divup.append(pup);
+    divup.append(temp);
+    divup.append(pup1);
     
-    
+    if (localStorage.getItem("APIkey") !== null)
+        idbtn.hide();
     
     function saveapikey() {
-        var apikey = $("#apikey").val();
-        localStorage.setItem("APIkey", apikey);
+        if (typeof(localStorage) === "undefined"){
+            alert("Local Storage not supported");
+        }
+        else { 
+            
+            var apikey = $("#apikey").val();
+            localStorage.setItem("APIkey", apikey);
+            if (localStorage.getItem("APIkey") !== null)
+                idbtn.hide();
+        }
     }
     
     function zipcode() {
-        var zipcode = $("#zip").val();
+        var zipcode = $.trim($("#zip").val());
         var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&units=imperial&appid=" + localStorage.getItem("APIkey");
         $.ajax( {
             url: url,
             success: function(data){
-                console.log(data.weather[0].main);
-                
-                var temper = data.main.temp;
-                divup.append($("<h2>").text("Weather Type"));
-                divup.append($("<p>").text(data.weather[0].main));
-                divup.append($("<h2>").text("Temperature (f)"));
-                divup.append($("<p>").text(temper));
+                if (data.cod === "404") {
+                    pup.text("Invalid Zip Code");
+                    pup1.text("Invalid Zip Code");
+                }
+                else {
+                    var temper = data.main.temp;
+                    pup.text(data.weather[0].description);
+                    pup1.text(temper);
+                }
             }
         })
     }
